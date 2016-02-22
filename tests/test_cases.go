309f7b3b3d36869e7cd59571caf908e84a5192ed
amorgun <alexander.morgun.usu@gmail.com>
2016-02-23 01:41:57 +0600

    Added failing tests

--- tests/test_cases.go
+++ tests/test_cases.go
@@ -1,6 +1,7 @@
 package tests
 
 import (
+	"time"
 	"github.com/amorgun/go-tour-crawler/crawler"
 )
 
@@ -57,6 +58,94 @@ func getTestCases() []testCase {
 				"http://golang.org/pkg/": "Packages",
 				"http://golang.org/pkg/fmt/": "Package fmt",
 				"http://golang.org/pkg/os/": "Package os",
+			},
+		},
+		testCase{
+			startUrl: "1",
+			maxDepth: 100,
+			fetcher: newWarningFetcher(fakeFetcher{
+				"0": &fakeResult{
+					"node #0",
+					[]string{},
+					func(){},
+				},
+				"1": &fakeResult{
+					"node #1",
+					[]string{},
+					func(){},
+				},
+				"2": &fakeResult{
+					"node #2",
+					[]string{},
+					func(){},
+				},
+			}),
+			expectedBodies: map[string]string {
+				"1": "node #1",
+			},
+		},
+		testCase{
+			startUrl: "0",
+			maxDepth: 4,
+			fetcher: newWarningFetcher(fakeFetcher{
+				"0": &fakeResult{
+					"node #0",
+					[]string{
+						"1",
+						"3",
+					},
+					func(){},
+				},
+				"1": &fakeResult{
+					"node #1 with very slow fetching",
+					[]string{
+						"2",
+					},
+					func(){
+						time.Sleep(1 * time.Second)
+					},
+				},
+				"2": &fakeResult{
+					"node #2",
+					[]string{
+						"5",
+						"6",
+					},
+					func(){},
+				},
+				"3": &fakeResult{
+					"node #3",
+					[]string{
+						"4",
+					},
+					func(){},
+				},
+				"4": &fakeResult{
+					"node #4",
+					[]string{
+						"2",
+					},
+					func(){},
+				},
+				"5": &fakeResult{
+					"node #5",
+					[]string{},
+					func(){},
+				},
+				"6": &fakeResult{
+					"node #6",
+					[]string{},
+					func(){},
+				},
+			}),
+			expectedBodies: map[string]string {
+				"0": "node #0",
+				"1": "node #1 with very slow fetching",
+				"2": "node #2",
+				"3": "node #3",
+				"4": "node #4",
+				"5": "node #5",
+				"6": "node #6",
 			},
 		},
 	}
